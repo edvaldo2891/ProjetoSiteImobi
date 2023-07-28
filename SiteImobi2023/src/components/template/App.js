26100032e953d58f126380e8a19995a66d0433c2
Edvaldo Antunes <edvaldo.ntns@gmail.com>
2023-07-28 20:41:29 -0300

    alteração do nome do projeto

--- SiteImobi2023/src/components/template/App.js
+++ SiteImobi2023/src/components/template/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
-// Componentes internos
 import Footer from './Footer';
 import NavBar from './NavBar';
 import ModalBanner from './ModalBanner';
 import FooterLGPD from './FooterLGPD';
-// import ModalLGPD from './ModalLGPD';
+
 
 
 function App() {
@@ -14,13 +12,11 @@ function App() {
   if(ativo){
     return (
       <div>
-        {console.log('Entrou no site')}
         <NavBar />
         <ModalBanner />
         <Outlet />
         <FooterLGPD />
         <Footer />
-        {/* <ModalLGPD /> */}
       </div>
     );
   }
